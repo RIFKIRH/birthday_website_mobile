e6c1e70773ca063d41578fe8f85dd7bb389ad506
Rifki Rahmatun Hidayah <95902634+RIFKIRH@users.noreply.github.com>
2025-02-09 12:08:14 +0700

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,10 @@
 import bg from './bg.jpg'
 
 import picture from './picture.png'
-
+import ici1 from './ici1.png'
+import ici2 from './ici2.png'
+import ici3 from './ici3.png'
+import ici4 from './ici4.png'
 import confetti from './confetti.gif'
 
 import CakeSVG from './svg/CakeSVG'
@@ -11,6 +14,10 @@ import CakeSVG from './svg/CakeSVG'
 export {
     bg,
     picture,
+    ici1,
+    ici2,
+    ici3,
+    ici4,
     confetti,
     CakeSVG
 }
